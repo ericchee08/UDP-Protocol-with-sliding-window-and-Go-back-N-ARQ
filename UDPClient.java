76ea781f7d2131a41a52d5f0ba60ae73844bb243
Eric Chee <58150120+ericchee08@users.noreply.github.com>
2021-10-30 20:37:19 +0100

    Add files via upload

--- UDPClient.java
+++ UDPClient.java
@@ -5,31 +5,37 @@ import java.util.Scanner;
 class UDPClient {
 	public static void main(String args[]) throws Exception {
 
+		// Read user input using scanner class
+		Scanner inputwindow = new Scanner(System.in);  
+		System.out.println("Client Ready.");
+		System.out.println("------------------------");
+		System.out.println("\nPlease enter window size: ");
+		int N = inputwindow.nextInt();
+		inputwindow.close(); //close the scanner class
 		
-		Scanner reader = new Scanner(System.in);  // Reading from System.in
-		System.out.println("Enter window size: ");
-		int N = reader.nextInt();
-		reader.close();
+		System.out.println("\nWINDOW SIZE = " + N);
 		
-		//variables
+		//sequence number variable - Pointer 2
 		int nextseqnum = 0;
+		//window slide tracker - Pointer 1
 		int send_base = 0;
-		// Window size
+		// timeout variable 
 		boolean timedOut = true;
 
-		//// Read input
+		//// Read file input
 		BufferedReader inFromUser = new BufferedReader(new FileReader("file.txt"));
 		// Create client socket
 		DatagramSocket clientSocket = new DatagramSocket();
-		// get hostname
+		// get host name
 		InetAddress IPAddress = InetAddress.getByName("localhost");
+		// Set timer for SocketException timeout
 		clientSocket.setSoTimeout(1000);
 
 		//create byte data 
 		byte[] sendData = new byte[1024];
 		byte[] receiveData = new byte[1024];
 
-		//string variable for user input
+		//Extract file word in this case "umbrella" into a string variable 
 		StringBuilder sb = new StringBuilder();
 	    String word = inFromUser.readLine();
 
@@ -39,45 +45,60 @@ class UDPClient {
 	        word = inFromUser.readLine();
 	    }
 	    String sentence = sb.toString();
-
+	    
+	    //Max variable equal to the length of the word within the file read
 		int max = sentence.length();
 
+		// sequence number comparison with max variable used to determine when to close the clientSocket
 		while ((nextseqnum < (max-1)) && timedOut) {
 			try {
+				//Fixed Sliding window size dependent on the user's window size input
 				if (nextseqnum < send_base + N) {
-
-					String seq = String.valueOf(nextseqnum + " "); // "0 "
+					
+					//Concatenating the sequence number and the file string character information together to be placed into the same packet
+					String seq = String.valueOf(nextseqnum + " "); // 
 					seq = seq.concat(sentence.substring(nextseqnum, nextseqnum + 1));
-
-					String packet = "SN = " + seq;
-					// convert user input into byte
+					//length of word in file included in the packet in order for the receiver to extract and use as while loop termination.
+					String packet = "SN = " + seq + " " + max;
+					
+					//Convert the packet containing both sequence number and string character into byte
 					sendData = packet.getBytes();
-
-					System.out.println("\n\n Sending Frame " + packet);
+					
+					//sending datagram packet to the Server through the client socket
+					System.out.println("\n\nSending Frame " + packet.substring(0,8));
 					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
 					clientSocket.send(sendPacket);
-
+					
+					//Receiving datagram packet (acknowledgement) from the server socket
 					DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 					clientSocket.receive(receivePacket);
-
-					String modifiedSentence = new String(receivePacket.getData());
-					System.out.println("\n\nACK RECEIVED");
-					System.out.println("FROM SERVER: " + modifiedSentence);
+					
+					//Succesfully received packets are printed on the client side as confirmation
+					String data = new String(receivePacket.getData());
+					System.out.println("\n\nACK Received");
+					System.out.println("From Server: " + data.substring(0,8));
+					
+					//increment sequence number when successfully received acknowledgement from server
 					nextseqnum++;
-
+					
+				// if the last frame has been sent on the sliding window, increment the sliding window position by one.
 				} else if (send_base < (max - N)) {
 					send_base++;
-					System.out.println("Slide position:" + send_base);
-
+					System.out.println("\n       - Sliding Window Position: " + send_base + " -");
 				}
+				
+				//a timeout is caught if acknowledgement is not received from the server.
 			} catch (SocketTimeoutException exception) {
-				// ACK not received therefore resend data packet.
-				System.out.println("\n\nTimeout (Sequence Number " + nextseqnum + ")");
-				System.out.println("Resending Frame: " + nextseqnum);
+				// ACK not received therefore re-send data packet and already acknowledged frames.
+				System.out.println("\n\n        Timeout (Sequence Number " + nextseqnum + ")");
+				System.out.println("        Retransmitting Frame(s)");
+				// sequence number equal to send base for retransmission of frames within the current window position 
 				nextseqnum = send_base;
 			}
 		}
-		System.out.println("All data sent. exiting.");
+		//While loop exited, close client and Buffered reader.
+		System.out.println("\n\n----------------------------------------------------------");
+		System.out.println("All data succesfully sent and acknowledged. Session Closed.");
 		clientSocket.close();
 		inFromUser.close();
 	}
--- UDPServer.java
+++ UDPServer.java
@@ -1,51 +1,73 @@
-import java.io.*;
 import java.net.*;
 import java.util.Random;
 
 class UDPServer {
 	public static void main(String args[]) throws Exception {
-
-		int nextAck = 0;
-
+		
+		//newAck variable keeps track of only new acknowledge/sequence numbers to be received to ensure reliable transfer 
+		int newAck_Seq = 0;
+		 //Max variable used to exit while loop - receives the string length from the client.
+		int max = 99999;
+		
+		//Create server socket
 		DatagramSocket serverSocket = new DatagramSocket(9876);
+		System.out.println("Server Ready: waiting for packets...");
+		System.out.println("------------------------------------");
+		
+		//create byte data 
 		byte[] receiveData = new byte[1024];
 		byte[] sendData = new byte[1024];
-		while (nextAck < 8) {
-			// Get the received packet
+		while (newAck_Seq < (max-1)) {
+			
+			//Get the transmitted packet from the client 
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 			serverSocket.receive(receivePacket);
+			
+			//Get packet's IP and port
+			InetAddress IPAddress = receivePacket.getAddress();
+			int port = receivePacket.getPort();
 
-			// Get the message from the packet
+			//Get the message from the packet
 			String sentence = new String(receivePacket.getData());
 			int SN = Integer.valueOf(sentence.substring(5, 6));
 
-			// random number generator in order to simulate a timeout event
+			//Random number generator in order to simulate a timeout event
 			Random random = new Random();
 			int timeout = random.nextInt(100);
-//			&& Integer.valueOf(sentence.substring(5, 6)) == SN)
-			if ((timeout > 30)) {
-//
-				if (SN == nextAck) {
-					System.out.println("\n\nReceived packet " + sentence);
+
+			//25% timeout rate
+			if ((timeout > 25)) {
+				//Extract word length from the packet and set as max variable for while loop exit.
+				max = Integer.valueOf(sentence.substring(9, 10));
+				
+				//Compare the sequence number with the ack/SN tracker, if the same increment by 1 for the next packet comparison.
+				if (SN == newAck_Seq) {
+					System.out.println("\n\nReceived packet " + sentence.substring(0,8));
 					System.out.println("ACK Sent");
-					nextAck++;
+					newAck_Seq++;
+					
+				//If the sequence number comparison is not the same on receiver and server, do not increment ack/seq tracker
 				} else {
-					System.out.println("Resent Packets Received: " + sentence.substring(5, 6));
+					System.out.println("        Retransmitted Frames Received: " + sentence.substring(5, 6));
 				}
-//				
-				InetAddress IPAddress = receivePacket.getAddress();
-				int port = receivePacket.getPort();
-				String capitalizedSentence = sentence.toUpperCase();
-				sendData = capitalizedSentence.getBytes();
+				
+				//Convert the packet containing both sequence number and string character into byte as ACK information.
+				String data = sentence;
+				sendData = data.getBytes();
 				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
 				serverSocket.send(sendPacket);
-
+				
+			//failure of receiving transmission from client results in packet drop/lost.
 			} else {
+				//Extract word length from the packet and set as max variable for while loop exit.
+				max = Integer.valueOf(sentence.substring(9, 10));
 				// package drop due to timeout, no acknowledgement sent.
-				System.out.println("\n\nPacket with " + sentence + " was dropped");
+				System.out.println("\n\n        Packet with " + sentence.substring(0,8) + " was dropped");
 			}
 		}
-		System.out.println("All data received. exiting.");
+		//While loop exited, close server session.
+		System.out.println("\n\n--------------------------------------");
+		System.out.println("All data received. exiting. Session Closed");
 		serverSocket.close();
 	}
 }
