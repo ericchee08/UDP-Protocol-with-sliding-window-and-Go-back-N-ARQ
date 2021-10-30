bc3af79d38ab8f9be1ebf1780535c160215a9659
Eric Chee <58150120+ericchee08@users.noreply.github.com>
2021-10-30 14:36:22 +0100

    Add files via upload

--- UDPClient.java
+++ UDPClient.java
@@ -1,27 +1,84 @@
-// AKS 2020 York Online Masters
-// --------UDPClient-----------
-// Socket Communications - UDP
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
+
 class UDPClient {
-public static void main(String args[]) throws Exception
-{
-BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-DatagramSocket clientSocket = new DatagramSocket();
-InetAddress IPAddress = InetAddress.getByName("localhost");
-byte[] sendData = new byte[1024];
-byte[] receiveData = new byte[1024];
-String sentence = inFromUser.readLine();
-sendData = sentence.getBytes();
-DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,IPAddress, 9876);
-clientSocket.send(sendPacket);
-DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length);
-clientSocket.receive(receivePacket);
-String modifiedSentence = new String(receivePacket.getData());
-System.out.println("FROM SERVER:  " + modifiedSentence);
-clientSocket.close();
+	public static void main(String args[]) throws Exception {
+
+		
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		System.out.println("Enter window size: ");
+		int N = reader.nextInt();
+		reader.close();
+		
+		//variables
+		int nextseqnum = 0;
+		int send_base = 0;
+		// Window size
+		boolean timedOut = true;
+
+		//// Read input
+		BufferedReader inFromUser = new BufferedReader(new FileReader("file.txt"));
+		// Create client socket
+		DatagramSocket clientSocket = new DatagramSocket();
+		// get hostname
+		InetAddress IPAddress = InetAddress.getByName("localhost");
+		clientSocket.setSoTimeout(1000);
+
+		//create byte data 
+		byte[] sendData = new byte[1024];
+		byte[] receiveData = new byte[1024];
+
+		//string variable for user input
+		StringBuilder sb = new StringBuilder();
+	    String word = inFromUser.readLine();
+
+	    while (word != null) {
+	        sb.append(word);
+	        sb.append(System.lineSeparator());
+	        word = inFromUser.readLine();
+	    }
+	    String sentence = sb.toString();
+
+		int max = sentence.length();
+
+		while ((nextseqnum < (max-1)) && timedOut) {
+			try {
+				if (nextseqnum < send_base + N) {
+
+					String seq = String.valueOf(nextseqnum + " "); // "0 "
+					seq = seq.concat(sentence.substring(nextseqnum, nextseqnum + 1));
+
+					String packet = "SN = " + seq;
+					// convert user input into byte
+					sendData = packet.getBytes();
+
+					System.out.println("\n\n Sending Frame " + packet);
+					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+					clientSocket.send(sendPacket);
+
+					DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+					clientSocket.receive(receivePacket);
+
+					String modifiedSentence = new String(receivePacket.getData());
+					System.out.println("\n\nACK RECEIVED");
+					System.out.println("FROM SERVER: " + modifiedSentence);
+					nextseqnum++;
+
+				} else if (send_base < (max - N)) {
+					send_base++;
+					System.out.println("Slide position:" + send_base);
+
+				}
+			} catch (SocketTimeoutException exception) {
+				// ACK not received therefore resend data packet.
+				System.out.println("\n\nTimeout (Sequence Number " + nextseqnum + ")");
+				System.out.println("Resending Frame: " + nextseqnum);
+				nextseqnum = send_base;
+			}
+		}
+		System.out.println("All data sent. exiting.");
+		clientSocket.close();
+		inFromUser.close();
 	}
 }
-
-
-
--- UDPServer.java
+++ UDPServer.java
@@ -1,28 +1,51 @@
-// ASK 2020 York Online Masters
-// ---------UDPServer ---------
-//  Socket communications UDP
 import java.io.*;
 import java.net.*;
-class UDPServer {
-public static void main(String args[]) throws Exception
-{
-	DatagramSocket serverSocket = new DatagramSocket(9876);
-	byte[] receiveData = new byte[1024];
-	byte[] sendData = new byte[1024];
-while(true)
-{
-	DatagramPacket receivePacket = new DatagramPacket(receiveData,
-	receiveData.length);
-	serverSocket.receive(receivePacket);
-	String sentence = new String(receivePacket.getData());
-	InetAddress IPAddress = receivePacket.getAddress();
+import java.util.Random;
 
-int port = receivePacket.getPort();
-String capitalizedSentence = sentence.toUpperCase();
-	sendData = capitalizedSentence.getBytes();
-DatagramPacket sendPacket = new DatagramPacket(sendData,
-	sendData.length, IPAddress, port);
-	serverSocket.send(sendPacket);
+class UDPServer {
+	public static void main(String args[]) throws Exception {
+
+		int nextAck = 0;
+
+		DatagramSocket serverSocket = new DatagramSocket(9876);
+		byte[] receiveData = new byte[1024];
+		byte[] sendData = new byte[1024];
+		while (nextAck < 8) {
+			// Get the received packet
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			serverSocket.receive(receivePacket);
+
+			// Get the message from the packet
+			String sentence = new String(receivePacket.getData());
+			int SN = Integer.valueOf(sentence.substring(5, 6));
+
+			// random number generator in order to simulate a timeout event
+			Random random = new Random();
+			int timeout = random.nextInt(100);
+//			&& Integer.valueOf(sentence.substring(5, 6)) == SN)
+			if ((timeout > 30)) {
+//
+				if (SN == nextAck) {
+					System.out.println("\n\nReceived packet " + sentence);
+					System.out.println("ACK Sent");
+					nextAck++;
+				} else {
+					System.out.println("Resent Packets Received: " + sentence.substring(5, 6));
+				}
+//				
+				InetAddress IPAddress = receivePacket.getAddress();
+				int port = receivePacket.getPort();
+				String capitalizedSentence = sentence.toUpperCase();
+				sendData = capitalizedSentence.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+				serverSocket.send(sendPacket);
+
+			} else {
+				// package drop due to timeout, no acknowledgement sent.
+				System.out.println("\n\nPacket with " + sentence + " was dropped");
+			}
+		}
+		System.out.println("All data received. exiting.");
+		serverSocket.close();
 	}
-  }
 }
